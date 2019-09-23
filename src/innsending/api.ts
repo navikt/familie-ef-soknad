ffad11760cfc5ed3884203b75241af57992c48f2
ma10s <mattis.janitz@nav.no>
2019-09-23 14:56:34 +0200

    Test - Send inn url => nais.oera-q.local

--- src/innsending/api.ts
+++ src/innsending/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const sendInnSøknad = (soknad: string) => axios
-    .get(`https://familie-ef-soknad-api.nais.oera-q.no/internal/status/isAlive`,  {
+    .get(`https://familie-ef-soknad-api.nais.oera-q.local/api/soknad/sendInn`,  {
         withCredentials: true
 })
     .then((response: { data: any; }) => {
