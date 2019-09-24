1481e4dcb05f5aed8ea84393d1c50aeb5ed02532
ma10s <mattis.janitz@nav.no>
2019-09-24 12:41:59 +0200

    Test - ping and => nais.oera-q.local

--- src/innsending/api.ts
+++ src/innsending/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const sendInnSøknad = (soknad: string) => axios
-    .post(`https://familie-ef-soknad-api.nais.oera-q.local/api/soknad/sendInn`, soknad, {
+    .post(`https://familie-ef-soknad-api.nais.oera-q.local/api/ping`, soknad, {
         headers: {"content-type": "application/json;charset=utf-8"},
         withCredentials: true
     })
