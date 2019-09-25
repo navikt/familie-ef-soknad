89af18e63f91879939a4565731995ce1ec30e3b4
ma10s <mattis.janitz@nav.no>
2019-09-25 15:52:53 +0200

    Test - POST on nais.oera-q.local

--- src/innsending/api.ts
+++ src/innsending/api.ts
@@ -1,38 +1,13 @@
 import axios from 'axios';
 
-const sendInnSøknad = (soknad: string) => axios
+const sendInnSøknad = (soknad: string) =>
+  axios
     .post(`https://familie-ef-soknad-api.nais.oera-q.local/api/ping`, soknad, {
-        headers: {"content-type": "application/json;charset=utf-8"},
-        withCredentials: true
+      headers: { 'content-type': 'application/json;charset=utf-8' },
+      withCredentials: true,
     })
     .then((response: { data: any }) => {
       return response.data;
     });
 
-const pingApi = () => axios
-    .get(`https://familie-ef-soknad-api.nais.oera-q.local/api/ping`,  {
-        withCredentials: true
-    })
-    .then((response: { data: any }) => {
-        return response.data;
-    });
-
-export { sendInnSøknad, pingApi };
-
-
-//     .get(`https://familie-ef-soknad-api.nais.oera-q.local/api/soknad/sendInn`,  {
-//         withCredentials: true
-// })
-
-// .post(`/api/soknad/sendInn`, soknad, {
-//     headers: {"content-type": "application/json;charset=utf-8"},
-//     withCredentials: true
-// })
-
-// .get(`https://soknad-kontantstotte-api-q.nav.no/api/tekster`,  {
-//     withCredentials: true
-// })
-//
-// .get(`https://familie-ef-soknad-api-q.nav.no/internal/status/isAlive`,  {
-//     withCredentials: true
-// })
+export { sendInnSøknad };
--- src/komponenter/Soknad.tsx
+++ src/komponenter/Soknad.tsx
@@ -1,57 +1,39 @@
 import React, { useState } from 'react';
 import { Hovedknapp } from 'nav-frontend-knapper';
-import {pingApi, sendInnSøknad} from '../innsending/api';
+import { sendInnSøknad } from '../innsending/api';
 
 interface SoknadProps {
   message: string;
 }
 
 interface IState {
-  status: string,
-  pingReturn: string;
+  status: string;
 }
 
 const Soknad: React.FC<SoknadProps> = ({ message }) => {
   const [hocState, setHocState] = useState<IState>({
-    status: `Søknad kan sendes`, pingReturn:""
+    status: `Søknad kan sendes`,
   });
-
-  const ping = () =>  {
-    pingApi()
-        .then((resultat) =>
-            setHocState({ ...hocState, pingReturn: ` ${resultat.text}` })
-        )
-        .catch((e) =>
-            setHocState({ ...hocState, pingReturn: `??????: ${e}` })
-        );
-  }
+  const send = () => {
+    let søknadsStreng = JSON.stringify({
+      text: message,
+    });
+    sendInnSøknad(søknadsStreng)
+      .then((resultat) =>
+        setHocState({ ...hocState, status: `Vi har kontakt: ${resultat}` })
+      )
+      .catch((e) =>
+        setHocState({ ...hocState, status: `Noe gikk galt: ${e}` })
+      );
+  };
 
   return (
     <>
       <h1>{message}</h1>
       <Hovedknapp onClick={send}>Send søknad</Hovedknapp>
       <p>Status: {hocState.status}</p>
-
-      <Hovedknapp onClick={ping}>Ping api</Hovedknapp>
-      <p>{hocState.pingReturn}</p>
-
     </>
   );
-
-
-
-  function send() {
-    let søknadsStreng = JSON.stringify({
-      text: message
-    });
-    sendInnSøknad(søknadsStreng)
-      .then((resultat) =>
-        setHocState({ ...hocState, status: `Vi har kontakt: ${resultat.text}` })
-      )
-      .catch((e) =>
-        setHocState({ ...hocState, status: `Noe gikk galt: ${e}` })
-      );
-  }
 };
 
 export default Soknad;
