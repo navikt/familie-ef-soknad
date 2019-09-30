82c99cd682456f0b21d1272e3529a762e7fb20ca
ma10s <mattis.janitz@nav.no>
2019-09-30 12:13:36 +0200

    Kjør circle-ci jobb for test-branch til prod-sbs.

--- src/innsending/api.ts
+++ src/innsending/api.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = window.location.origin;
+
 const sendInnSøknad = (soknad: string) =>
   axios
-    .post(`https://familie-ef-soknad-api.nais.oera-q.local/api/ping`, soknad, {
+    .post(`/api/ping`, soknad, {
       headers: { 'content-type': 'application/json;charset=utf-8' },
       withCredentials: true,
     })
