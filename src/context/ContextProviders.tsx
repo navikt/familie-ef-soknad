75584a52f2111dcc863bff78da9553773f78f5f0
My Thao Nguyen <mythng94@gmail.com>
2019-10-09 13:08:24 +0200

    legger til mal for global context vi kan bruke senere :D

---
+++ src/context/ContextProviders.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const ContextProviders: React.FC = ({ children }) => {
+  console.log('kake');
+  return <h1>cake</h1>;
+  /*return (
+
+    {children}
+
+  );*/
+};
+
+export default ContextProviders;
