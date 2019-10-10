627213b686edb48f2d5e364fc3ab4ef6b7404e48
Nina Kjekstad <ninakjeks@gmail.com>
2019-10-10 13:57:47 +0200

    test med baseurls

--- src/toggles/api.ts
+++ src/toggles/api.ts
@@ -1,13 +1,19 @@
 import axios from 'axios';
 import { IToggleName } from './types';
 
+if (window.location.hostname.indexOf('nais.oera-q.local') > -1)
+  axios.defaults.baseURL = 'https://familie-ef-soknad-api.nais.oera-q.local';
+else if (window.location.hostname.indexOf('nais.oera.no') > -1)
+  axios.defaults.baseURL = 'https://familie-ef-soknad-api.nais.oera.no';
+else {
+  axios.defaults.baseURL = 'http://localhost:8091/';
+}
+
 const hentToggles = () => {
   const toggleNames = Object.values(IToggleName).join('&feature=');
-  return axios
-    .get(`https://unleashproxy.nais.oera.no/api/feature?feature=${toggleNames}`)
-    .then((response) => {
-      return response.data;
-    });
+  return axios.get(`/api/feature?feature=${toggleNames}`).then((response) => {
+    return response.data;
+  });
 };
 // inject https://unleashproxy.nais.oera.no/api/
 
