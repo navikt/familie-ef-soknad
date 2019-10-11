b27e1e1d43d349059d5fdd18ebf7bd42bab0940c
Nina Kjekstad <ninakjeks@gmail.com>
2019-10-11 08:38:06 +0200

    test strategi

--- src/toggles/api.ts
+++ src/toggles/api.ts
@@ -3,9 +3,11 @@ import { IToggleName } from './types';
 
 const hentToggles = () => {
   const toggleNames = Object.values(IToggleName).join('&feature=');
-  return axios.get(`/api/feature?feature=${toggleNames}`).then((response) => {
-    return response.data;
-  });
+  return axios
+    .get(`/api/feature?feature=${toggleNames}&test=hei`)
+    .then((response) => {
+      return response.data;
+    });
 };
 
 export default hentToggles;
