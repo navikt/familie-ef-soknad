1a4c2f8411d28e0d33ced61003698b25bfa6766f
ma10s <mattis.janitz@nav.no>
2019-10-31 16:00:59 +0100

    Fjern sanity (cors issues)

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import Feilside from './komponenter/Feilside';
 import NavFrontendSpinner from 'nav-frontend-spinner';
-import Spørsmål from './komponenter/Spørsmal';
 import Søknad from './komponenter/Søknad';
-import { client } from './utils/sanity';
 import { Panel } from 'nav-frontend-paneler';
 import hentToggles from './toggles/api';
 import { ToggleName, Toggles } from './typer/toggles';
 
 const App = () => {
-  const [spørsmal, settSpørsmal] = useState<any>([]);
   const [toggles, settToggles] = useState<Toggles>({});
   const [fetching, settFetching] = useState<boolean>(true);
   const [error, settError] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = () => {
-      client
-        .fetch('*[_type == $type]', { type: 'sporsmal' })
-        .then((res: any) => {
-          settSpørsmal(res);
-        })
-        .catch((err: any) => {
-          settError(true);
-        });
-      hentToggles(settToggles);
+      hentToggles(settToggles).catch((err: any) => {
+        settError(true);
+      });
       settFetching(false);
     };
     fetchData();
   }, []);
 
-  const erSpørsmålDataHentet = spørsmal && spørsmal.length;
-
   if (!fetching) {
-    if (!error && erSpørsmålDataHentet) {
+    if (!error) {
       return (
         <div className="app">
           <Panel className="innholdspanel">
-            <div>
-              {toggles[ToggleName.vis_innsending] && <Søknad />}
-              <Spørsmål sporsmalListe={spørsmal} steg={1} />
-            </div>
+            <div>{toggles[ToggleName.vis_innsending] && <Søknad />}</div>
           </Panel>
         </div>
       );
