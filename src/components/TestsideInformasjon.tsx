56bce4593c72cdf6137e2a71f69735585804bee4
My Thao Nguyen <mythng94@gmail.com>
2019-12-06 14:13:47 +0100

    fikser testinfoside

--- src/components/TestsideInformasjon.tsx
+++ src/components/TestsideInformasjon.tsx
@@ -7,20 +7,9 @@ import styled from 'styled-components';
 
 const StyledTestside = styled.div`
   padding: 0 50px 0 50px;
-
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: auto-fit minmax();
-
-  .testside__tittel {
-    grid-column-start: 1;
-    grid-column-end: 3;
-  }
-  .testside__beskrivelse {
-  }
-
-  .testside__lenker {
-  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const TestsideInformasjon = () => {
@@ -28,28 +17,24 @@ const TestsideInformasjon = () => {
     <Panel className={'innholdspanel'}>
       <Veilederpanel fargetema="advarsel" type={'plakat'} kompakt svg={svg}>
         <StyledTestside>
-          <h1 className={'testside__tittel'}>
-            Dette er en testside som er under utvikling
-          </h1>
-          <p className={'testside__beskrivelse'}>
+          <h1>Dette er en testside som er under utvikling</h1>
+          <p>
             Hvis du kom til denne siden fordi du hadde behov for informasjon om
             stønader til enslig mor eller far, kan du finne mer informasjon om
             dette her:
           </p>
-          <div className={'testside__lenker'}>
-            <p>
-              <a
-                href={
-                  'https://www.nav.no/no/Person/Familie/Enslig+mor+eller+far/Nyttig+a+vite/oversikt-over-st%C3%B8nader-til-enslig-mor-eller-far'
-                }
-              >
-                Oversikt over stønader til enslig mor eller far
-              </a>
-            </p>
-            <p>
-              <a href={Environment().veiviserUrl}>Veiviser</a>
-            </p>
-          </div>
+          <p>
+            <a
+              href={
+                'https://www.nav.no/no/Person/Familie/Enslig+mor+eller+far/Nyttig+a+vite/oversikt-over-st%C3%B8nader-til-enslig-mor-eller-far'
+              }
+            >
+              Oversikt over stønader til enslig mor eller far
+            </a>
+          </p>
+          <p>
+            <a href={Environment().veiviserUrl}>Veiviser</a>
+          </p>
         </StyledTestside>
       </Veilederpanel>
     </Panel>
