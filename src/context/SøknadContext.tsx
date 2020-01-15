2a63981bf008e7303f99000d2e40b7d37199cea5
My Thao Nguyen <mythng94@gmail.com>
2020-01-15 13:57:51 +0100

    fikser på context

--- src/context/SøknadContext.tsx
+++ src/context/SøknadContext.tsx
@@ -30,9 +30,6 @@ const reducer = (state: ISøknad, action: IAction): ISøknad => {
 // -----------  CONTEXT  -----------
 const initialState: ISøknad = {
   person: mockPerson,
-  søkerOppholderSegINorge: undefined,
-  søkerBosattINorgeSisteTreÅr: undefined,
-  søkerErFlyktning: undefined,
 };
 
 const useSøknad = () => {
