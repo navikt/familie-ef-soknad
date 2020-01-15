cee5a3ddc2c49bcb85e799a5d7ac1edfade2fc9b
My Thao Nguyen <mythng94@gmail.com>
2020-01-15 18:53:30 +0100

    legger til oppsett for språkendring av datovelger

--- src/components/dato/Datovelger.tsx
+++ src/components/dato/Datovelger.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { registerLocale, setDefaultLocale } from 'react-datepicker';
+import nb from 'date-fns/locale/nb';
+import nn from 'date-fns/locale/nn';
+import en from 'date-fns/locale/en-US';
 import { Normaltekst } from 'nav-frontend-typografi';
 import LocaleTekst from '../../language/LocaleTekst';
 import FeltGruppe from '../FeltGruppe';
 import KalenderIkonSVG from '../../assets/KalenderSVG';
 import { addDays, subDays } from 'date-fns';
 import { dagensDato } from '../../utils/dato';
+import { useSpråkContext } from '../../context/SpråkContext';
 
 export enum DatoBegrensning {
   AlleDatoer = 'AlleDatoer',
@@ -27,10 +32,24 @@ const Datovelger: React.FC<Props> = ({
   valgtDato,
   settDato,
 }) => {
+  const [locale] = useSpråkContext();
+
+  const settLocaleForDatePicker = () => {
+    locale === 'nn'
+      ? registerLocale('nn', nn)
+      : locale === 'nb'
+      ? registerLocale('nb', nb)
+      : registerLocale('en-US', en);
+  };
+
   useEffect(() => {
     settDato(dagensDato);
+    setDefaultLocale('nb');
+
     // eslint-disable-next-line
   }, []);
+
+  settLocaleForDatePicker();
 
   return (
     <div className={'datovelger'}>
@@ -42,26 +61,29 @@ const Datovelger: React.FC<Props> = ({
           <div className={'datepicker__container'}>
             {datobegrensning === DatoBegrensning.TidligereDatoer ? (
               <DatePicker
+                className={'datovelger__input'}
                 onChange={(e) => settDato(e)}
                 selected={valgtDato !== undefined ? valgtDato : dagensDato}
                 dateFormat={'dd.MM.yyyy'}
+                locale={locale}
                 maxDate={addDays(new Date(), 0)}
-                className={'datovelger__input'}
               />
             ) : datobegrensning === DatoBegrensning.FremtidigeDatoer ? (
               <DatePicker
+                className={'datovelger__input'}
                 onChange={(e) => settDato(e)}
                 selected={valgtDato !== undefined ? valgtDato : dagensDato}
                 dateFormat={'dd.MM.yyyy'}
                 minDate={subDays(new Date(), 0)}
-                className={'datovelger__input'}
+                locale={locale}
               />
             ) : datobegrensning === DatoBegrensning.AlleDatoer ? (
               <DatePicker
+                className={'datovelger__input'}
                 onChange={(e) => settDato(e)}
                 selected={valgtDato !== undefined ? valgtDato : dagensDato}
                 dateFormat={'dd.MM.yyyy'}
-                className={'datovelger__input'}
+                locale={locale}
               />
             ) : null}
           </div>
