32b82c7dbbc8c7fba995afd585a7c812d9db7e84
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-01-16 14:27:02 +0100

    Test komponent

--- src/søknad/inngangsvilkår/omdeg/OmDeg.tsx
+++ src/søknad/inngangsvilkår/omdeg/OmDeg.tsx
@@ -6,6 +6,7 @@ import Side from '../../../components/side/Side';
 import { IRoute, Routes } from '../../../config/Routes';
 import { hentNesteRoute } from '../../../utils/routing';
 import { useLocation } from 'react-router';
+import { Filopplaster } from '../../../components/filopplaster/Filopplaster';
 
 const OmDeg: FC = () => {
   const location = useLocation();
@@ -20,6 +21,7 @@ const OmDeg: FC = () => {
       <Personopplysninger />
       <Sivilstatus />
       <Medlemskap />
+      <Filopplaster title="Test" />
     </Side>
   );
 };
