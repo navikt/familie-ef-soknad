34930f8c6d694c8ecfd6e531a485c7459072defe
My Thao Nguyen <mythng94@gmail.com>
2020-01-17 14:28:40 +0100

    fikser merge conflicts

--- src/søknad/inngangsvilkår/omdeg/OmDeg.tsx
+++ src/søknad/inngangsvilkår/omdeg/OmDeg.tsx
@@ -1,18 +1,19 @@
 import React, { FC } from 'react';
 import Personopplysninger from './Personopplysninger';
-import Sivilstatus from './Sivilstatus';
-import Medlemskap from './Medlemskap';
+import Sivilstatus from './sivilstatus/Sivilstatus';
+import Medlemskap from './medlemskap/Medlemskap';
 import Side from '../../../components/side/Side';
 import { IRoute, Routes } from '../../../config/Routes';
 import { hentNesteRoute } from '../../../utils/routing';
 import { useLocation } from 'react-router';
+import { IntlShape, injectIntl } from 'react-intl';
 
-const OmDeg: FC = () => {
+const OmDeg: FC<{ intl: IntlShape }> = ({ intl }) => {
   const location = useLocation();
   const nesteRoute: IRoute = hentNesteRoute(Routes, location.pathname);
   return (
     <Side
-      tittel={'Om deg'}
+      tittel={intl.formatMessage({ id: 'stegtittel.omDeg' })}
       nestePath={nesteRoute.path}
       tilbakePath={Routes[0].path}
     >
@@ -23,4 +24,4 @@ const OmDeg: FC = () => {
   );
 };
 
-export default OmDeg;
+export default injectIntl(OmDeg);
