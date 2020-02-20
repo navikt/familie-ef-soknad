ab9a4424d3ff9f26ccd008272486d06d633ed92c
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-02-20 09:51:06 +0100

    Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App = () => {
   useEffect(() => {
     const fetchData = () => {
       hentToggles(settToggles).catch((err: Error) => {
-        settError(false);
+        settError(true);
       });
 
       const fetchPersonData = () => {
@@ -61,7 +61,6 @@ const App = () => {
           <TestsideInformasjon />
           <Switch>
             <Route path={'/'}>
-              <Søknadsdialog />
               {toggles[ToggleName.vis_innsending] && <Søknadsdialog />}
             </Route>
           </Switch>
