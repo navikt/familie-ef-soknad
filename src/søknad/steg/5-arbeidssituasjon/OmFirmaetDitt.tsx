993b41ec3bc07ee9e6b616d9d937acfde9992188
My Thao Nguyen <mythng94@gmail.com>
2020-02-26 10:32:20 +0100

    Legger til inputfelter, datovelger og fritekstfelt

--- src/søknad/steg/5-arbeidssituasjon/OmFirmaetDitt.tsx
+++ src/søknad/steg/5-arbeidssituasjon/OmFirmaetDitt.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Undertittel } from 'nav-frontend-typografi';
-import { IArbeidssituasjon } from '../../../models/arbeidssituasjon';
+import { IArbeidssituasjon, IFirma } from '../../../models/arbeidssituasjon';
 import { useIntl } from 'react-intl';
-import { Input } from 'nav-frontend-skjema';
+import { Input, Textarea } from 'nav-frontend-skjema';
 import Datovelger, {
   DatoBegrensning,
 } from '../../../components/dato/Datovelger';
@@ -17,18 +17,67 @@ const OmFirmaetDitt: React.FC<Props> = ({
   settArbeidssituasjon,
 }) => {
   const intl = useIntl();
-  const { firma } = arbeidssituasjon;
+  const [firma, settFirma] = useState<IFirma>({});
+
+  useEffect(() => {
+    settArbeidssituasjon({ ...arbeidssituasjon, firma: firma });
+    // eslint-disable-next-line
+  }, [firma]);
+
+  const settDatoFelt = (dato: Date | null): void => {
+    dato !== null &&
+      settFirma({
+        ...firma,
+        etableringsdato: { label: 'datovelger tekstid', verdi: dato },
+      });
+  };
+
+  const settArbeidsukeTekst = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ): void => {
+    settFirma({
+      ...firma,
+      arbeidsuke: { label: 'tekstid', verdi: e.target.value },
+    });
+  };
+
+  const settInputTekstFelt = (
+    e: React.FormEvent<HTMLInputElement>,
+    nøkkel: 'navn' | 'organisasjonsnummer'
+  ) => {
+    settFirma({
+      ...firma,
+      [nøkkel]: { label: '', verdi: e.currentTarget.value },
+    });
+  };
 
   return (
     <>
       <Undertittel className={'sentrert'}>Tittel</Undertittel>
-      <Input label={'Navn på firma'} bredde={'L'} type={'text'} />
-      <Input label={'Organisasjonsnummer'} bredde={'L'} type={'text'} />
+      <Input
+        label={'Navn på firma'}
+        bredde={'L'}
+        type={'text'}
+        onChange={(e) => settInputTekstFelt(e, 'navn')}
+      />
+      <Input
+        label={'Organisasjonsnummer'}
+        bredde={'L'}
+        type={'text'}
+        onChange={(e) => settInputTekstFelt(e, 'organisasjonsnummer')}
+      />
       <Datovelger
         valgtDato={firma?.etableringsdato?.verdi}
         tekstid={'datovelger tekstid'}
         datobegrensning={DatoBegrensning.TidligereDatoer}
-        settDato={}
+        settDato={(e) => settDatoFelt(e)}
+      />
+      Sett inn InputOgLabelGruppe her
+      <Textarea
+        label={'arbeidsuke tekstid'}
+        value={firma.arbeidsuke?.verdi ? firma.arbeidsuke?.verdi : ''}
+        maxLength={1000}
+        onChange={(e) => settArbeidsukeTekst(e)}
       />
     </>
   );
