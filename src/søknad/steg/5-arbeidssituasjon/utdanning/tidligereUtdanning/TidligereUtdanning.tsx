e7f13602cb6a7cf6e99e8a0a51bbb6e6488fbdf4
My Thao Nguyen <mythng94@gmail.com>
2020-03-03 16:10:19 +0100

    Legger til JaNeiSpørsmål i TidligereUtdanning.tsx

--- src/søknad/steg/5-arbeidssituasjon/utdanning/tidligereUtdanning/TidligereUtdanning.tsx
+++ src/søknad/steg/5-arbeidssituasjon/utdanning/tidligereUtdanning/TidligereUtdanning.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SeksjonGruppe from '../../../../../components/gruppe/SeksjonGruppe';
 import { Undertittel } from 'nav-frontend-typografi';
 import LocaleTekst from '../../../../../language/LocaleTekst';
 import KomponentGruppe from '../../../../../components/gruppe/KomponentGruppe';
+import { ITidligereUtdanning } from '../../../../../models/arbeidssituasjon/utdanning';
+import JaNeiSpørsmål from '../../../../../components/spørsmål/JaNeiSpørsmål';
+import { utdanningEtterGrunnskolenSpm } from './TidligereUtdanningConfig';
+import { ISpørsmål } from '../../../../../models/spørsmal';
+import { hentTekst } from '../../../../../utils/søknad';
+import { useIntl } from 'react-intl';
 
 const TidligereUtdanning: React.FC = () => {
+  const [tidligereUtdanning, settTidligereUtdanning] = useState<
+    ITidligereUtdanning
+  >({});
+  const intl = useIntl();
+
+  const settJaNeiSpørsmål = (spørsmål: ISpørsmål, svar: boolean) => {
+    tidligereUtdanning &&
+      settTidligereUtdanning({
+        ...tidligereUtdanning,
+        [spørsmål.søknadid]: {
+          label: hentTekst(spørsmål.tekstid, intl),
+          verdi: svar,
+        },
+      });
+  };
+
   return (
     <SeksjonGruppe>
       <KomponentGruppe>
         <Undertittel className={'sentrert'}>
           <LocaleTekst tekst={'utdanning.tittel.tidligere'} />
         </Undertittel>
+      </KomponentGruppe>
+
+      <KomponentGruppe>
+        <JaNeiSpørsmål
+          spørsmål={utdanningEtterGrunnskolenSpm}
+          onChange={settJaNeiSpørsmål}
+          valgtSvar={tidligereUtdanning?.tattUtdanningEtterGrunnskole?.verdi}
+        />
       </KomponentGruppe>
     </SeksjonGruppe>
   );
