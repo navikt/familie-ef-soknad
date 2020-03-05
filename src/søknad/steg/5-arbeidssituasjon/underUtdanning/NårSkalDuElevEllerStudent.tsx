6724147eac4ee0117df1ac59b659bfa626280072
My Thao Nguyen <mythng94@gmail.com>
2020-03-05 12:33:33 +0100

    Setter tomPeriode ved første render by default

--- src/søknad/steg/5-arbeidssituasjon/underUtdanning/NårSkalDuElevEllerStudent.tsx
+++ src/søknad/steg/5-arbeidssituasjon/underUtdanning/NårSkalDuElevEllerStudent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { IUnderUtdanning } from '../../../../models/arbeidssituasjon/utdanning';
 import PeriodeDatovelgere from '../../../../components/dato/PeriodeDatovelger';
 import { tomPeriode } from '../../../../utils/søknadsfelter';
@@ -12,6 +12,11 @@ const NårSkalDuVæreElevEllerStudent: React.FC<Props> = ({
   utdanning,
   settUtdanning,
 }) => {
+  useEffect(() => {
+    settUtdanning({ ...utdanning, periode: tomPeriode });
+    // eslint-disable-next-line
+  }, []);
+
   const settPeriode = (dato: Date | null, nøkkel: string): void => {
     utdanning.periode &&
       settUtdanning({
