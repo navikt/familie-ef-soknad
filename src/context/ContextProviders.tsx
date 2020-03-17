0e144d7b13d88eaabc03368891fd1f0edcdb1ac8
My Thao Nguyen <mythng94@gmail.com>
2020-03-17 13:37:22 +0100

    Gjør ContextProvider mer lettleselig

--- src/context/ContextProviders.tsx
+++ src/context/ContextProviders.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import useSøknadContext from './SøknadContext';
+import { SøknadProvider } from './SøknadContext';
 import { PersonProvider } from './PersonContext';
 
 const ContextProviders: React.FC = ({ children }) => {
   return (
     <PersonProvider>
-      <useSøknadContext.Provider>{children}</useSøknadContext.Provider>
+      <SøknadProvider>{children}</SøknadProvider>
     </PersonProvider>
   );
 };
