8461bffaec7acd2f5c6fd37c130a31b42128181a
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-03-30 14:47:40 +0200

    Legg til Arbeidsgiver i Oppsummering

--- src/søknad/steg/7-oppsummering/OppsummeringAktiviteter.tsx
+++ src/søknad/steg/7-oppsummering/OppsummeringAktiviteter.tsx
@@ -3,9 +3,30 @@ import useSøknadContext from '../../../context/SøknadContext';
 import { Element, Normaltekst } from 'nav-frontend-typografi';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import { VisLabelOgSvar } from '../../../utils/visning';
+import { hentTekst } from '../../../utils/søknad';
+import { useIntl, IntlShape } from 'react-intl';
+
+const VisArbeidsforhold = (arbeidsforhold: any[], intl: IntlShape) => {
+  return arbeidsforhold.map((arbeid, index) => {
+    let tekst = hentTekst('arbeidsforhold.tittel.arbeidsgiver', intl);
+
+    if (arbeidsforhold.length > 1) {
+      tekst = tekst + ' ' + (index + 1);
+    }
+
+    return (
+      <div className="arbeidsforhold">
+        <Element>{tekst}</Element>
+        {VisLabelOgSvar(arbeid)}
+        {index < arbeidsforhold.length - 1 && <hr />}
+      </div>
+    );
+  });
+};
 
 const OppsummeringAktiviteter: React.FC = () => {
   const { søknad } = useSøknadContext();
+  const intl = useIntl();
 
   const aktivitet = søknad.aktivitet;
 
@@ -13,9 +34,17 @@ const OppsummeringAktiviteter: React.FC = () => {
 
   const firma = aktivitet.firma ? VisLabelOgSvar(aktivitet.firma) : null;
 
+  const arbeidsforhold = aktivitet.arbeidsforhold
+    ? VisArbeidsforhold(aktivitet.arbeidsforhold, intl)
+    : null;
+
   return (
-    <Ekspanderbartpanel tittel="Arbeid, utdanning og mindre aktiviteter">
+    <Ekspanderbartpanel
+      className="aktiviteter"
+      tittel="Arbeid, utdanning og mindre aktiviteter"
+    >
       {arbeidssituasjon}
+      {arbeidsforhold}
       {firma}
     </Ekspanderbartpanel>
   );
