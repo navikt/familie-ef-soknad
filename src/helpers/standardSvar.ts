4879103517d34df515570ee706c5d95c6ddaa7b8
My Thao Nguyen <mythng94@gmail.com>
2020-04-03 09:41:30 +0200

    Fikser riktig JaNeiSvar

--- src/helpers/standardSvar.ts
+++ src/helpers/standardSvar.ts
@@ -1,9 +1,9 @@
-import { ESvar, ISvar } from '../models/spørsmålogsvar';
+import { ESvar, ESvarTekstid, ISvar } from '../models/spørsmålogsvar';
 
 export const JaNeiSvar: ISvar[] = [
-  { id: ESvar.JA, svar_tekstid: ESvar.JA },
-  { id: ESvar.NEI, svar_tekstid: ESvar.NEI },
+  { id: ESvar.JA, svar_tekstid: ESvarTekstid.JA },
+  { id: ESvar.NEI, svar_tekstid: ESvarTekstid.NEI },
 ];
 
-export const NeiSvar: ISvar = { id: ESvar.NEI, svar_tekstid: ESvar.NEI };
-export const JaSvar: ISvar = { id: ESvar.JA, svar_tekstid: ESvar.JA };
+export const NeiSvar: ISvar = { id: ESvar.NEI, svar_tekstid: ESvarTekstid.NEI };
+export const JaSvar: ISvar = { id: ESvar.JA, svar_tekstid: ESvarTekstid.JA };
