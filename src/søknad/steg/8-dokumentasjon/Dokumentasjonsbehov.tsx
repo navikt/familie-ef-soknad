c5e952b5ca0b942ae68c3d1997f69447c658f977
My Thao Nguyen <mythng94@gmail.com>
2020-04-20 14:00:41 +0200

    Legger til vedleggopplaster på dokbehov
    - gjør checkbox funksjonell for "harSendtInn"
    - Vedleggsopplaster må testes i prod

--- src/søknad/steg/8-dokumentasjon/Dokumentasjonsbehov.tsx
+++ src/søknad/steg/8-dokumentasjon/Dokumentasjonsbehov.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { FormattedHTMLMessage, useIntl } from 'react-intl';
-import KomponentGruppe from '../../../components/gruppe/KomponentGruppe';
-import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
+import React, { useEffect, useState } from 'react';
+import FeltGruppe from '../../../components/gruppe/FeltGruppe';
+import Filopplaster from '../../../components/filopplaster/Filopplaster';
+import LocaleTekst from '../../../language/LocaleTekst';
 import SeksjonGruppe from '../../../components/gruppe/SeksjonGruppe';
 import { Checkbox } from 'nav-frontend-skjema';
-import FeltGruppe from '../../../components/gruppe/FeltGruppe';
+import { FormattedHTMLMessage, useIntl } from 'react-intl';
 import { hentTekst } from '../../../utils/søknad';
 import { IDokumentasjon } from '../../../models/dokumentasjon';
-import LocaleTekst from '../../../language/LocaleTekst';
+import { IVedlegg } from '../../../models/vedlegg';
+import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
+import { useSøknad } from '../../../context/SøknadContext';
 
 interface Props {
   dokumentasjon: IDokumentasjon;
@@ -15,6 +17,38 @@ interface Props {
 
 const Dokumentasjonsbehov: React.FC<Props> = ({ dokumentasjon }) => {
   const intl = useIntl();
+  const { søknad, settSøknad } = useSøknad();
+  const [dokumentasjonsbehov, settDokumentasjon] = useState<IDokumentasjon[]>(
+    søknad.dokumentasjonsbehov
+  );
+
+  useEffect(() => {
+    settSøknad({ ...søknad, dokumentasjonsbehov: dokumentasjonsbehov });
+  }, [dokumentasjonsbehov]);
+
+  const settVedlegg = (vedleggliste: IVedlegg[]) => {
+    const dokumentasjonMedVedlegg = dokumentasjonsbehov.map((dok) => {
+      if (dok.id === dokumentasjon.id) {
+        return {
+          ...dok,
+          opplastedeVedlegg: vedleggliste,
+        };
+      } else return dok;
+    });
+    settDokumentasjon(dokumentasjonMedVedlegg);
+  };
+
+  const settHarSendtInnTidligere = (e: any) => {
+    const dokumentasjonEndret = dokumentasjonsbehov.map((dok) => {
+      if (dok.id === dokumentasjon.id) {
+        return {
+          ...dok,
+          harSendtInn: e.target.checked,
+        };
+      } else return dok;
+    });
+    settDokumentasjon(dokumentasjonEndret);
+  };
 
   return (
     <SeksjonGruppe>
@@ -31,10 +65,18 @@ const Dokumentasjonsbehov: React.FC<Props> = ({ dokumentasjon }) => {
       <FeltGruppe>
         <Checkbox
           label={hentTekst('dokumentasjon.checkbox.sendtTidligere', intl)}
+          checked={dokumentasjon.harSendtInn}
+          onChange={settHarSendtInnTidligere}
         />
       </FeltGruppe>
-
-      <KomponentGruppe>Sett inn vedleggsopplaster her</KomponentGruppe>
+      <Filopplaster
+        settVedlegg={settVedlegg}
+        vedleggsliste={
+          dokumentasjon.opplastedeVedlegg ? dokumentasjon.opplastedeVedlegg : []
+        }
+        tittel={hentTekst(dokumentasjon.tittel, intl)}
+        dokumentasjonsType={hentTekst(dokumentasjon.tittel, intl)}
+      />
     </SeksjonGruppe>
   );
 };
