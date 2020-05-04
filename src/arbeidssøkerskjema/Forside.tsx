0d83dcb9a7f7f93b30b035d9d4de2f5dbf6839b0
My Thao Nguyen <mythng94@gmail.com>
2020-05-04 12:46:22 +0200

    Fjern import

--- src/arbeidssøkerskjema/Forside.tsx
+++ src/arbeidssøkerskjema/Forside.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Panel } from 'nav-frontend-paneler';
 import FeltGruppe from '../components/gruppe/FeltGruppe';
 import { BekreftCheckboksPanel } from 'nav-frontend-skjema';
-import { Element, Normaltekst, Sidetittel } from 'nav-frontend-typografi';
+import { Element, Sidetittel } from 'nav-frontend-typografi';
 import { usePersonContext } from '../context/PersonContext';
 import { useSpråkContext } from '../context/SpråkContext';
 import { hentBeskjedMedNavn } from '../utils/språk';
