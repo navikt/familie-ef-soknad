6875086e8187241b2bd37177e4bccd4a53d0c1fb
My Thao Nguyen <mythng94@gmail.com>
2020-05-15 16:38:37 +0200

    Begrens visning for AndreForelder "infogruppe"
    - refaktorerer til styled-components (bytter til grid)
    - Fjerner ubrukt less kode etter refaktorering
    - Legg til disabled property i datovelger

--- src/søknad/steg/4-barnasbosted/OmAndreForelder.tsx
+++ src/søknad/steg/4-barnasbosted/OmAndreForelder.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import KomponentGruppe from '../../../components/gruppe/KomponentGruppe';
-import FeltGruppe from '../../../components/gruppe/FeltGruppe';
+
 import { Input } from 'nav-frontend-skjema';
 import { Checkbox } from 'nav-frontend-skjema';
 import Datovelger, {
@@ -8,12 +7,41 @@ import Datovelger, {
 } from '../../../components/dato/Datovelger';
 import { IBarn } from '../../../models/barn';
 import { IForelder } from '../../../models/forelder';
+import styled from 'styled-components/macro';
+import KomponentGruppe from '../../../components/gruppe/KomponentGruppe';
 
 interface Props {
   barn: IBarn;
   settForelder: Function;
   forelder: IForelder;
 }
+
+const StyledAndreForelderGruppe = styled.div`
+  display: grid;
+  min-width: 500px;
+  grid-template-columns: repeat(3, min-content);
+  grid-template-rows: repeat(3, min-content);
+  grid-gap: 1rem;
+  grid-template-areas: 'navn navn' 'fødselsdato personnr' 'checkbox checkbox';
+
+  .foreldre-navn-input {
+    grid-area: navn;
+  }
+
+  .datovelger {
+    grid-area: fødselsdato;
+  }
+  .personnummer {
+    grid-area: personnr;
+    min-width: 300px;
+    .skjemaelement__label {
+      margin-bottom: 1rem;
+    }
+  }
+  .checkbox {
+    grid-area: checkbox;
+  }
+`;
 const OmAndreForelder: React.FC<Props> = ({ settForelder, forelder }) => {
   const [huketAv, settHuketAv] = useState<boolean>(false);
 
@@ -32,72 +60,72 @@ const OmAndreForelder: React.FC<Props> = ({ settForelder, forelder }) => {
   };
 
   return (
-    <>
-      <KomponentGruppe>
-        <FeltGruppe>
-          <Input
-            className="foreldre-navn-input"
-            onChange={(e) =>
-              settForelder({
-                ...forelder,
-                navn: {
-                  label: 'halla',
-                  verdi: e.target.value,
-                },
-              })
-            }
-            value={forelder.navn ? forelder.navn?.verdi : ''}
-            label="Navn"
-            disabled={huketAv}
-          />
-        </FeltGruppe>
-      </KomponentGruppe>
-      <KomponentGruppe>
-        <div className="fødselsnummer">
-          <Datovelger
-            settDato={(e: Date | null) => {
-              e !== null &&
+    <KomponentGruppe>
+      <StyledAndreForelderGruppe>
+        <Input
+          className="foreldre-navn-input"
+          onChange={(e) =>
+            settForelder({
+              ...forelder,
+              navn: {
+                label: 'halla',
+                verdi: e.target.value,
+              },
+            })
+          }
+          value={forelder.navn ? forelder.navn?.verdi : ''}
+          label="Navn"
+          disabled={huketAv}
+        />
+
+        {forelder.navn && (
+          <>
+            <Datovelger
+              disabled={huketAv}
+              settDato={(e: Date | null) => {
+                e !== null &&
+                  settForelder({
+                    ...forelder,
+                    flyttetFra: {
+                      label: 'Fødselsnummer datotest',
+                      verdi: e,
+                    },
+                  });
+              }}
+              valgtDato={
+                forelder.fødselsdato && forelder.fødselsdato.verdi
+                  ? forelder.fødselsdato.verdi
+                  : undefined
+              }
+              tekstid={'datovelger.fødselsdato'}
+              datobegrensning={DatoBegrensning.TidligereDatoer}
+            />
+            <Input
+              className="personnummer"
+              onChange={(e) =>
                 settForelder({
                   ...forelder,
-                  flyttetFra: {
-                    label: 'Fødselsnummer datotest',
-                    verdi: e,
+                  personnr: {
+                    label: 'Personnr',
+                    verdi: e.target.value,
                   },
-                });
-            }}
-            valgtDato={
-              forelder.fødselsdato && forelder.fødselsdato.verdi
-                ? forelder.fødselsdato.verdi
-                : undefined
-            }
-            tekstid={'datovelger.fødselsdato'}
-            datobegrensning={DatoBegrensning.TidligereDatoer}
-          />
-          <Input
-            className="personnummer"
-            onChange={(e) =>
-              settForelder({
-                ...forelder,
-                personnr: {
-                  label: 'Personnr',
-                  verdi: e.target.value,
-                },
-              })
-            }
-            value={forelder.personnr ? forelder.personnr?.verdi : ''}
-            label="Personnummer (hvis barnet har fått)"
-            disabled={huketAv}
-          />
-        </div>
-        <FeltGruppe classname="checkbox-forelder">
-          <Checkbox
-            label={'Jeg kan ikke oppgi den andre forelderen'}
-            checked={huketAv}
-            onChange={hukAv}
-          />
-        </FeltGruppe>
-      </KomponentGruppe>
-    </>
+                })
+              }
+              value={forelder.personnr ? forelder.personnr?.verdi : ''}
+              label="Personnummer (hvis barnet har fått)"
+              disabled={huketAv}
+            />
+          </>
+        )}
+
+        <Checkbox
+          className={'checkbox'}
+          label={'Jeg kan ikke oppgi den andre forelderen'}
+          checked={huketAv}
+          onChange={hukAv}
+        />
+      </StyledAndreForelderGruppe>
+    </KomponentGruppe>
   );
 };
 
