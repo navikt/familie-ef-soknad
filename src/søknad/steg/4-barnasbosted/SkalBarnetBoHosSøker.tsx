7301c392ca9f7de430d3283f82bece2a06a41927
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-05-29 14:05:14 +0200

    Legg til fil med nytt navn

---
+++ src/søknad/steg/4-barnasbosted/SkalBarnetBoHosSøker.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import KomponentGruppe from '../../../components/gruppe/KomponentGruppe';
+import { useIntl } from 'react-intl';
+import { skalBarnetBoHosSøker } from './ForeldreConfig';
+import FeltGruppe from '../../../components/gruppe/FeltGruppe';
+import AlertStripe from 'nav-frontend-alertstriper';
+import MultiSvarSpørsmål from '../../../components/spørsmål/MultiSvarSpørsmål';
+import { Normaltekst } from 'nav-frontend-typografi';
+import { hentTekst } from '../../../utils/søknad';
+import LocaleTekst from '../../../language/LocaleTekst';
+import { IBarn } from '../../../models/barn';
+import { ISpørsmål, ISvar } from '../../../models/spørsmålogsvar';
+import { useSøknad } from '../../../context/SøknadContext';
+
+interface Props {
+  barn: IBarn;
+  forelder: any;
+  settForelder: Function;
+}
+
+const SkalBarnetBoHosSøker: React.FC<Props> = ({
+  barn,
+  forelder,
+  settForelder,
+}) => {
+  const intl = useIntl();
+  const { settDokumentasjonsbehov } = useSøknad();
+
+  const settSkalBarnetBoHosSøkerFelt = (spørsmål: ISpørsmål, svar: ISvar) => {
+    settForelder({
+      ...forelder,
+      [skalBarnetBoHosSøker.søknadid]: {
+        label: intl.formatMessage({
+          id: 'barnasbosted.spm.skalBarnetBoHosSøker',
+        }),
+        verdi: hentTekst(svar.svar_tekstid, intl),
+      },
+    });
+    settDokumentasjonsbehov(spørsmål, svar);
+  };
+
+  return (
+    <>
+      {barn.harSammeAdresse ? (
+        <KomponentGruppe>
+          <AlertStripe type={'advarsel'} form={'inline'}>
+            <LocaleTekst tekst={'barnasbosted.alert.måBoHosDeg'} />
+          </AlertStripe>
+          <MultiSvarSpørsmål
+            key={skalBarnetBoHosSøker.søknadid}
+            spørsmål={skalBarnetBoHosSøker}
+            valgtSvar={forelder.skalBarnetBoHosSøker?.verdi}
+            settSpørsmålOgSvar={settSkalBarnetBoHosSøkerFelt}
+          />
+        </KomponentGruppe>
+      ) : null}
+      {forelder.skalBarnetBoHosSøker ===
+      intl.formatMessage({ id: 'barnasbosted.spm.jaMenSamarbeiderIkke' }) ? (
+        <FeltGruppe>
+          <AlertStripe type={'info'} form={'inline'}>
+            <LocaleTekst tekst={'barnasbosted.alert.hvisFaktiskBor'} />
+          </AlertStripe>
+          <FeltGruppe>
+            <Normaltekst className="innskutt">
+              Familievernkontoret kan også hjelpe deg
+            </Normaltekst>
+          </FeltGruppe>
+          <Normaltekst className="innskutt">
+            Når det kommer til denne søknaden kan du dokumentere at Lise bor hos
+            deg ved å sende inn for eksempel:
+          </Normaltekst>
+          <ul className="dokumentere-bosted">
+            <li>
+              <Normaltekst>
+                redegjørelse for årsaken til manglende adresseendring for barnet
+              </Normaltekst>
+            </li>
+            <li>
+              <Normaltekst>
+                kopi av flyttemelding/tips til Folkeregisteret
+              </Normaltekst>
+            </li>
+            <li>
+              <Normaltekst>
+                bekreftelse fra for eksempel barnehage/skole, barnevern eller
+                helsestasjon
+              </Normaltekst>
+            </li>
+          </ul>
+        </FeltGruppe>
+      ) : null}
+    </>
+  );
+};
+
+export default SkalBarnetBoHosSøker;
