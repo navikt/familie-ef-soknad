9e425d7b86205d11bb7f46878b664c992ce3b52a
Mattis Janitz <mattis.janitz@nav.no>
2020-06-05 09:34:59 +0200

    Ikke bruk mock-data som fallback. (#145)
    * Ikke vis mockdata(barn) i andre miljøer enn dev selv om barneliste ikke hentes
    + muliggjør "tving bruk av api i dev"
    + Test enkel løsning for ikke bygge på branch
    
    * Deploy hvis pullrequest i dev.
    
    * Deploy hvis pullrequest i dev.
    
    * Deploy hvis pullrequest i dev.

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,9 @@ const App = () => {
 
   autentiseringsInterceptor();
 
-  const erIDev = process.env.NODE_ENV === 'development';
+  const kallApi = () =>
+    process.env.NODE_ENV !== 'development' ||
+    process.env.REACT_APP_BRUK_API_I_DEV === 'true';
 
   useEffect(() => {
     verifiserAtBrukerErAutentisert(settAutentisering);
@@ -57,7 +59,7 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    let mapBarn = !erIDev && barneliste ? barneliste : mockPersonMedBarn.barn;
+    let mapBarn = kallApi() ? barneliste : mockPersonMedBarn.barn;
 
     const barnMedLabels = mapBarn.map((barn: any) => {
       const nyttBarn = settLabelOgVerdi(barn, standardLabelsBarn);
