cb4d70484b7a6e0684d866cb2adaee412264c939
My Thao Nguyen <mythng94@gmail.com>
2020-06-23 09:51:02 +0200

    Gjem SkrivUtKvittering-knappen inntil videre

--- src/søknad/steg/9-kvittering/DineSaker.tsx
+++ src/søknad/steg/9-kvittering/DineSaker.tsx
@@ -1,23 +1,24 @@
 import React, { FC } from 'react';
 import SeksjonGruppe from '../../../components/gruppe/SeksjonGruppe';
-import FeltGruppe from '../../../components/gruppe/FeltGruppe';
 import { Normaltekst } from 'nav-frontend-typografi';
 import LocaleTekst from '../../../language/LocaleTekst';
+import KomponentGruppe from '../../../components/gruppe/KomponentGruppe';
 
 const DineSaker: FC = () => {
   return (
     <SeksjonGruppe>
-      <FeltGruppe>
+      <KomponentGruppe>
         <Normaltekst>
           <LocaleTekst tekst={'kvittering.tekst.dineSaker'} />
         </Normaltekst>
-      </FeltGruppe>
-      <a
-        className={'knapp knapp--standard kvittering'}
-        href={'https://www.nav.no/soknader/nb/person/arbeid/tilleggsstonader'}
-      >
-        <LocaleTekst tekst={'kvittering.knapp.dineSaker'} />
-      </a>
+      </KomponentGruppe>
+      {false && (
+        <KomponentGruppe>
+          <a className={'knapp knapp--standard kvittering'} href={''}>
+            <LocaleTekst tekst={'kvittering.knapp.dineSaker'} />
+          </a>
+        </KomponentGruppe>
+      )}
     </SeksjonGruppe>
   );
 };
