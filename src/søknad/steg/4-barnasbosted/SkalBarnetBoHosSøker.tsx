4fff51544f950f4b4fac7229a0a30b33cceed710
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-06-24 11:08:00 +0200

    Fjern ubrukt import

--- src/søknad/steg/4-barnasbosted/SkalBarnetBoHosSøker.tsx
+++ src/søknad/steg/4-barnasbosted/SkalBarnetBoHosSøker.tsx
@@ -18,7 +18,6 @@ import {
 import { FormattedHTMLMessage } from 'react-intl';
 import { ESkalBarnetBoHosSøker } from '../../../models/steg/barnasbosted';
 import AlertStripeDokumentasjon from '../../../components/AlertstripeDokumentasjon';
-import LocaleTekst from '../../../language/LocaleTekst';
 
 interface Props {
   barn: IBarn;
