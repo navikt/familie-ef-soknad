72f744e3bcf1d45b7ed09a1a705f192d3c3fd9f0
My Thao Nguyen <mythng94@gmail.com>
2020-07-28 10:09:50 +0200

    Prep gjenbruk av steg 8 dokumentasjon og 9 kvittering

---
+++ src/barnetilsyn/steg/8-dokumentasjon/SendBarnetilsynSøknad.tsx
@@ -0,0 +1,114 @@
+import React, { FC } from 'react';
+import KnappBase from 'nav-frontend-knapper';
+import LocaleTekst from '../../../language/LocaleTekst';
+import { IStatus } from '../../../arbeidssøkerskjema/innsending/typer';
+import { ISøknad } from '../../../models/søknad';
+import { parseISO } from 'date-fns';
+import { useSøknad } from '../../../context/SøknadContext';
+import { useHistory, useLocation } from 'react-router';
+import KomponentGruppe from '../../../components/gruppe/KomponentGruppe';
+import AlertStripe from 'nav-frontend-alertstriper';
+import { Normaltekst } from 'nav-frontend-typografi';
+import SeksjonGruppe from '../../../components/gruppe/SeksjonGruppe';
+import { StyledKnapper } from '../../../arbeidssøkerskjema/komponenter/StyledKnapper';
+import { hentForrigeRoute, hentNesteRoute } from '../../../routing/utils';
+import { Routes } from '../../../routing/Routes';
+import {
+  mapBarnTilEntenIdentEllerFødselsdato,
+  sendInnSøknad,
+} from '../../../innsending/api';
+
+interface Innsending {
+  status: string;
+  melding: string;
+  venter: boolean;
+}
+
+const SendSøknadKnapper: FC = () => {
+  const { søknad, settSøknad } = useSøknad();
+  const location = useLocation();
+  const history = useHistory();
+  const nesteRoute = hentNesteRoute(Routes, location.pathname);
+  const forrigeRoute = hentForrigeRoute(Routes, location.pathname);
+
+  const [innsendingState, settinnsendingState] = React.useState<Innsending>({
+    status: IStatus.KLAR_TIL_INNSENDING,
+    melding: `Søknad kan sendes`,
+    venter: false,
+  });
+
+  const sendSøknad = (søknad: ISøknad) => {
+    const barnMedEntenIdentEllerFødselsdato = mapBarnTilEntenIdentEllerFødselsdato(
+      søknad.person.barn
+    );
+
+    const søknadMedFiltrerteBarn: ISøknad = {
+      ...søknad,
+      person: { ...søknad.person, barn: barnMedEntenIdentEllerFødselsdato },
+    };
+    settinnsendingState({ ...innsendingState, venter: true });
+    sendInnSøknad(søknadMedFiltrerteBarn)
+      .then((kvittering) => {
+        settinnsendingState({
+          ...innsendingState,
+          status: IStatus.SUKSESS,
+          melding: `Vi har kontakt: ${kvittering.text}`,
+          venter: false,
+        });
+        settSøknad({
+          ...søknad,
+          innsendingsdato: parseISO(kvittering.mottattDato),
+        });
+        history.push(nesteRoute.path);
+      })
+      .catch((e) =>
+        settinnsendingState({
+          ...innsendingState,
+          status: IStatus.FEILET,
+          melding: `Noe gikk galt: ${e}`,
+          venter: false,
+        })
+      );
+  };
+
+  return (
+    <>
+      {innsendingState.status === IStatus.FEILET && (
+        <KomponentGruppe>
+          <AlertStripe type={'advarsel'} form={'inline'}>
+            <Normaltekst>{innsendingState.melding}</Normaltekst>
+          </AlertStripe>
+        </KomponentGruppe>
+      )}
+      <SeksjonGruppe className={'sentrert'}>
+        <StyledKnapper>
+          <KnappBase
+            className={'tilbake'}
+            type={'standard'}
+            onClick={() => history.push(forrigeRoute.path)}
+          >
+            <LocaleTekst tekst={'knapp.tilbake'} />
+          </KnappBase>
+
+          <KnappBase
+            type={'hoved'}
+            onClick={() => !innsendingState.venter && sendSøknad(søknad)}
+            className={'neste'}
+            spinner={innsendingState.venter}
+          >
+            <LocaleTekst tekst={'knapp.sendSøknad'} />
+          </KnappBase>
+          <KnappBase
+            className={'avbryt'}
+            type={'flat'}
+            onClick={() => history.push(Routes[0].path)}
+          >
+            <LocaleTekst tekst={'knapp.avbryt'} />
+          </KnappBase>
+        </StyledKnapper>
+      </SeksjonGruppe>
+    </>
+  );
+};
+
+export default SendSøknadKnapper;
