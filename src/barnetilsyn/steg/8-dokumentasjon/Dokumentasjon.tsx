59bae726b7a0ef0180b07bd7d9da556fdead4b3b
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-08-18 10:04:15 +0200

    Fiks imports

--- src/barnetilsyn/steg/8-dokumentasjon/Dokumentasjon.tsx
+++ src/barnetilsyn/steg/8-dokumentasjon/Dokumentasjon.tsx
@@ -1,11 +1,8 @@
 import React, { useEffect } from 'react';
-import KomponentGruppe from '../../../components/gruppe/KomponentGruppe';
-import Lenke from 'nav-frontend-lenker';
 import SeksjonGruppe from '../../../components/gruppe/SeksjonGruppe';
-import { ESvar } from '../../../models/felles/spørsmålogsvar';
 import { FormattedHTMLMessage, useIntl } from 'react-intl';
 import { hentTekst, unikeDokumentasjonsbehov } from '../../../utils/søknad';
-import { Element, Normaltekst } from 'nav-frontend-typografi';
+import { Normaltekst } from 'nav-frontend-typografi';
 import { useLocation } from 'react-router-dom';
 import { usePrevious } from '../../../utils/hooks';
 import LastOppVedlegg from '../../../søknad/steg/8-dokumentasjon/LastOppVedlegg';
@@ -19,7 +16,7 @@ const Dokumentasjon: React.FC = () => {
   const intl = useIntl();
   const { søknad, settSøknad, mellomlagreBarnetilsyn } = useBarnetilsynSøknad();
   const location = useLocation();
-  const { aktivitet, dokumentasjonsbehov } = søknad;
+  const { dokumentasjonsbehov } = søknad;
   const sidetittel: string = hentTekst('dokumentasjon.tittel', intl);
   const forrigeDokumentasjonsbehov = usePrevious(søknad.dokumentasjonsbehov);
 
