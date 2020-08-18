040ca9f37d9d6a91455162d47ec965acb8cd4dec
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-08-18 10:02:44 +0200

    Fjern json-print før innsending

--- src/barnetilsyn/steg/8-dokumentasjon/Dokumentasjon.tsx
+++ src/barnetilsyn/steg/8-dokumentasjon/Dokumentasjon.tsx
@@ -83,13 +83,6 @@ const Dokumentasjon: React.FC = () => {
             );
           })}
       </SeksjonGruppe>
-      <div>
-        <Element>
-          JSON som sendes inn (kopier denne og send til utviklere dersom
-          innsending feiler):
-        </Element>
-        <pre>{JSON.stringify(søknad, null, 2)}</pre>
-      </div>
       <SendSøknadKnapper />
     </Side>
   );
