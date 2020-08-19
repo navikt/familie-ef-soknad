a2663f9391a69a05cc17119209c713938a6b1cf3
Johan Blomgren <johan.blomgren@nav.no>
2020-08-19 18:07:58 +0200

    Static

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const app = express();
 const port = process.env.PORT || 8080;
 const proxy = require('http-proxy-middleware');
 
-app.use(express.static(path.join(__dirname, 'build'), { index: false }));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use('/', function(req, res, next) {
+/*app.use('/', function(req, res, next) {
   console.log(`Server: A new request received at ${process.env.NODE_ENV}`);
   next();
 });
+ */
 
 // proxy
 app.use(
