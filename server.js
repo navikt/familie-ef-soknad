47c8390a88fdee85b24dd43434550decb4752b4d
Johan Blomgren <johan.blomgren@nav.no>
2020-08-19 18:48:14 +0200

    Static

--- server.js
+++ server.js
@@ -2,51 +2,22 @@ var dotenv = require('dotenv');
 dotenv.config();
 var path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8080;
-const proxy = require('http-proxy-middleware');
+const router = express.Router();
 
-app.use(express.static(path.join(__dirname, 'build'), { index: false }));
+router.use(express.static(path.join(__dirname, 'build'), { index: false }));
 
-/*app.use('/', function(req, res, next) {
-  console.log(`Server: A new request received at ${process.env.NODE_ENV}`);
-  next();
+router.get('/status', (req, res) => {
+  res.status(200).end();
 });
- */
-
-// proxy
-app.use(
-  '/api',
-  proxy('/api', {
-    changeOrigin: true,
-    logLevel: 'info',
-    target: `${
-      process.env.NODE_ENV === 'local'
-        ? 'http://localhost:8091'
-        : //: 'http://familie-ef-soknad-api'
-          'http://familie-ef-soknad-api.dev-sbs.nais.io'
-    }`,
-    secure: true,
-  })
-);
-
-app.get('/ping', (req, res) => {
-  res.send({ express: ' Ack :) ' });
+app.get('/status', (req, res) => {
+  res.status(200).end();
 });
-// Nais functions
-app.get(`/internal/isAlive|isReady`, (req, res) => res.sendStatus(200));
 
-app.get('/', function(req, res) {
-  console.log(`/`);
+router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build/', 'index.html'));
 });
 
-app.get('/familie/alene-med-barn/soknad', function(req, res) {
-  console.log(`/familie/alene-med-barn/soknad`);
-  res.sendFile(path.join(__dirname, 'build/', 'index.html'));
-});
-
+app.use('/familie/alene-med-barn/soknad', router);
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-console.log('Node env: ', process.env.NODE_ENV);
