fb7c84db3b743f6171ffa01325aa015e1e0da25e
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-08-19 21:38:29 +0200

    Fjern undødvendig tom komponent

--- src/søknad/steg/5-aktivitet/underUtdanning/Studiekostnader.tsx
+++ src/søknad/steg/5-aktivitet/underUtdanning/Studiekostnader.tsx
@@ -46,74 +46,70 @@ const Studiekostnader: React.FC<Props> = ({ utdanning, oppdaterUtdanning }) => {
   );
 
   return (
-    <>
-      <KomponentGruppe>
-        <StyledUndertittel>
-          <LocaleTekst tekst={'utdanning.label.utgifter'} />
-        </StyledUndertittel>
-        <FeltGruppe>
-          <InputLabelGruppe
-            label={semesteravgiftLabel}
-            nøkkel={EUtdanning.semesteravgift}
-            type={'number'}
-            bredde={'XS'}
-            settInputFelt={(e) =>
-              settInputFelt(EUtdanning.semesteravgift, semesteravgiftLabel, e)
-            }
-            beskrivendeTekst={'kroner'}
-            value={
-              utdanning?.semesteravgift?.verdi
-                ? utdanning?.semesteravgift?.verdi
-                : ''
-            }
-          />
-        </FeltGruppe>
-        <FeltGruppe>
-          <InputLabelGruppe
-            label={studieavgiftLabel}
-            nøkkel={EUtdanning.studieavgift}
-            type={'number'}
-            bredde={'XS'}
-            settInputFelt={(e) =>
-              settInputFelt(EUtdanning.studieavgift, studieavgiftLabel, e)
-            }
-            beskrivendeTekst={'kroner'}
-            value={
-              utdanning?.studieavgift?.verdi
-                ? utdanning?.studieavgift?.verdi
-                : ''
-            }
-          />
-        </FeltGruppe>
-        <FeltGruppe>
-          <InputLabelGruppe
-            label={eksamensgebyrLabel}
-            nøkkel={EUtdanning.eksamensgebyr}
-            type={'number'}
-            bredde={'XS'}
-            settInputFelt={(e) =>
-              settInputFelt(EUtdanning.eksamensgebyr, eksamensgebyrLabel, e)
-            }
-            beskrivendeTekst={'kroner'}
-            value={
-              utdanning?.eksamensgebyr?.verdi
-                ? utdanning?.eksamensgebyr?.verdi
-                : ''
-            }
-          />
-        </FeltGruppe>
-        <FeltGruppe>
-          <AlertStripeDokumentasjon>
-            {hentTekst('utdanning.andreUtgifter.fakturatekst', intl)}
-          </AlertStripeDokumentasjon>
-        </FeltGruppe>
-        <FeltGruppe>
-          <AlertStripeInfo className={'fjernBakgrunn'}>
-            <FormattedHTMLMessage id={'utdanning.andreUtgifter.tekst'} />
-          </AlertStripeInfo>
-        </FeltGruppe>
-      </KomponentGruppe>
-    </>
+    <KomponentGruppe>
+      <StyledUndertittel>
+        <LocaleTekst tekst={'utdanning.label.utgifter'} />
+      </StyledUndertittel>
+      <FeltGruppe>
+        <InputLabelGruppe
+          label={semesteravgiftLabel}
+          nøkkel={EUtdanning.semesteravgift}
+          type={'number'}
+          bredde={'XS'}
+          settInputFelt={(e) =>
+            settInputFelt(EUtdanning.semesteravgift, semesteravgiftLabel, e)
+          }
+          beskrivendeTekst={'kroner'}
+          value={
+            utdanning?.semesteravgift?.verdi
+              ? utdanning?.semesteravgift?.verdi
+              : ''
+          }
+        />
+      </FeltGruppe>
+      <FeltGruppe>
+        <InputLabelGruppe
+          label={studieavgiftLabel}
+          nøkkel={EUtdanning.studieavgift}
+          type={'number'}
+          bredde={'XS'}
+          settInputFelt={(e) =>
+            settInputFelt(EUtdanning.studieavgift, studieavgiftLabel, e)
+          }
+          beskrivendeTekst={'kroner'}
+          value={
+            utdanning?.studieavgift?.verdi ? utdanning?.studieavgift?.verdi : ''
+          }
+        />
+      </FeltGruppe>
+      <FeltGruppe>
+        <InputLabelGruppe
+          label={eksamensgebyrLabel}
+          nøkkel={EUtdanning.eksamensgebyr}
+          type={'number'}
+          bredde={'XS'}
+          settInputFelt={(e) =>
+            settInputFelt(EUtdanning.eksamensgebyr, eksamensgebyrLabel, e)
+          }
+          beskrivendeTekst={'kroner'}
+          value={
+            utdanning?.eksamensgebyr?.verdi
+              ? utdanning?.eksamensgebyr?.verdi
+              : ''
+          }
+        />
+      </FeltGruppe>
+      <FeltGruppe>
+        <AlertStripeDokumentasjon>
+          {hentTekst('utdanning.andreUtgifter.fakturatekst', intl)}
+        </AlertStripeDokumentasjon>
+      </FeltGruppe>
+      <FeltGruppe>
+        <AlertStripeInfo className={'fjernBakgrunn'}>
+          <FormattedHTMLMessage id={'utdanning.andreUtgifter.tekst'} />
+        </AlertStripeInfo>
+      </FeltGruppe>
+    </KomponentGruppe>
   );
 };
 
