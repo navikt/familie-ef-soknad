40c41e7079c65cc202f91a0fc0169e75218e3ca7
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-12-07 16:51:13 +0100

    Legg til funksjoner for å logge svar på spørsmål

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -14,6 +14,58 @@ export function logEvent(eventName: string, eventProperties: any) {
   amplitudeInstance.logEvent(eventName, eventProperties);
 }
 
+export const logSpørsmålBesvart = (
+  skjemanavn: string,
+  skjemaId: number,
+  spørsmål: string,
+  svar: string,
+  props?: any
+) => {
+  logEvent('skjema_spørsmål_besvart', {
+    skjemanavn,
+    skjemaId,
+    spørsmål,
+    svar,
+    ...props,
+  });
+};
+
+export const logSpørsmålBesvartOvergangsstønad = (
+  spørsmål: string,
+  svar: string
+) => {
+  const skjemanavn = 'Overgangsstønad';
+  const skjemaId = 150001;
+  logSpørsmålBesvart(skjemanavn, skjemaId, spørsmål, svar);
+};
+
+export const logSpørsmålBesvartArbeidssokerskjema = (
+  spørsmål: string,
+  svar: string
+) => {
+  const skjemanavn = 'Arbeidssøker';
+  const skjemaId = 150801;
+  logSpørsmålBesvart(skjemanavn, skjemaId, spørsmål, svar);
+};
+
+export const logSpørsmålBesvartBarnetilsyn = (
+  spørsmål: string,
+  svar: string
+) => {
+  const skjemanavn = 'Barnetilsyn';
+  const skjemaId = 150002;
+  logSpørsmålBesvart(skjemanavn, skjemaId, spørsmål, svar);
+};
+
+export const logSpørsmålBesvartSkolepenger = (
+  spørsmål: string,
+  svar: string
+) => {
+  const skjemanavn = 'Skolepenger';
+  const skjemaId = 150004;
+  logSpørsmålBesvart(skjemanavn, skjemaId, spørsmål, svar);
+};
+
 export const logSidevisningOvergangsstonad = (side: string) => {
   logEvent('sidevisning', {
     side,
