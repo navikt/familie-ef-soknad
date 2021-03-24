a330aa95dfcd24fe0235b89d2793dbc69fc30471
Asbjørn Steinskog <asbjorn@steinskog.me>
2021-03-24 13:31:35 +0100

    Fjern ubrukt kode

--- src/søknad/steg/3-barnadine/LeggTilBarn.tsx
+++ src/søknad/steg/3-barnadine/LeggTilBarn.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import JaNeiSpørsmål from '../../../components/spørsmål/JaNeiSpørsmål';
 import KomponentGruppe from '../../../components/gruppe/KomponentGruppe';
-import LeggTilBarnFødt from './LeggTilBarnFødt';
 import LeggTilBarnUfødt from './LeggTilBarnUfødt';
 import Seksjonsgruppe from '../../../components/gruppe/SeksjonGruppe';
 import { barnetFødt } from './BarneConfig';
@@ -43,7 +41,6 @@ const LeggTilBarn: React.FC<Props> = ({
   const [navn, settNavn] = useState('');
   const [ident, settIdent] = useState<string>('');
   const [boHosDeg, settBoHosDeg] = useState<string>('');
-  const [kjennerIkkeIdent, settKjennerIkkeIdent] = useState<boolean>(false);
   const [skalHaBarnepass, settSkalHaBarnepass] = useState<boolean | undefined>(
     true
   );
@@ -70,13 +67,6 @@ const LeggTilBarn: React.FC<Props> = ({
 
   const settBo = (event: any) => {
     settBoHosDeg(event.target.value);
-  };
-
-  const tilbakestillFelt = () => {
-    settBarnDato(undefined);
-    settNavn('');
-    settIdent('');
-    settBoHosDeg('');
   };
 
   const leggTilEllerEndreBarn = (id: string | undefined) => {
