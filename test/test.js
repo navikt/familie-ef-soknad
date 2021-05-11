53a2113174ed34b4db97710139037f658841ecc9
My Thao Nguyen <my.thao.nguyen@nav.no>
2021-05-11 14:44:23 +0200

    Endre navn

--- test/test.js
+++ test/test.js
@@ -1,25 +1,22 @@
-import Steg1 from './steg/steg1.js';
-import Steg2 from './steg/steg2.js';
-import Steg3 from './steg/steg3.js';
-import Steg4 from './steg/steg4.js';
-import Steg5 from './steg/steg5.js';
-import Steg6 from './steg/steg6.js';
-import Steg7 from './steg/steg7.js';
-import Steg8 from './steg/steg8.js';
+import Steg1_OmDeg from './steg/steg1_omdeg.js';
+import Steg2_Bosituasjon from './steg/steg2_bosituasjon.js';
+import Steg3_BarnaDine from './steg/steg3_barnadine.js';
+import Steg4_BarnasBosted from './steg/steg4_barnasbosted.js';
+import Steg5_Aktivitet from './steg/steg5_aktivitet.js';
+import Steg6_DinSituasjon from './steg/steg6_dinsituasjon.js';
+import Steg7_Oppsummering from './steg/steg7_oppsummering.js';
+import Steg8_Dokumentasjon from './steg/steg8_dokumentasjon.js';
 
 fixture`Overgangsstønad`
   .page`http://localhost:3000/familie/alene-med-barn/soknad`;
 
 test('Send inn minimal søknad', async (t) => {
-  await Steg1(t);
-
-  await Steg2(t);
-
-  await Steg3(t);
-
-  await Steg4(t);
-  await Steg5(t);
-  await Steg6(t);
-  await Steg7(t);
-  await Steg8(t);
+  await Steg1_OmDeg(t);
+  await Steg2_Bosituasjon(t);
+  await Steg3_BarnaDine(t);
+  await Steg4_BarnasBosted(t);
+  await Steg5_Aktivitet(t);
+  await Steg6_DinSituasjon(t);
+  await Steg7_Oppsummering(t);
+  await Steg8_Dokumentasjon(t);
 });
