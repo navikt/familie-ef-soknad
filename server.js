e948933f396b79198edb55ae63ffd1583f7cb5f7
alexandraback <alexandra1.back@gmail.com>
2021-06-14 19:18:39 +0200

    Begynner på en backend for frontend

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const path = require('path');
+const { injectDecoratorServerSide } = require("@navikt/nav-dekoratoren-moduler/ssr");
+const buildPath = path.resolve(__dirname, "../build");
+const basePath = "/person/personopplysninger";
+
 const app = express();
+
+const getHtmlWithDecorator = (filePath) =>
+    injectDecoratorServerSide({
+      env: process.env.ENV,
+      filePath: filePath});
 
 app.use(express.static(path.join(__dirname, 'build')));
 
