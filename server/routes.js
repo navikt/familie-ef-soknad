927c3dd8ab7c774f60598e59e4386eae183d64ab
alexandraback <alexandra1.back@gmail.com>
2021-07-08 17:04:07 +0200

    Legg på logging

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,4 @@
-const express  = require('express');
+const express = require('express');
 const path = require('path');
 const getHtmlWithDecorator = require("./decorator");
 const buildPath = path.resolve(__dirname, "../build");
@@ -7,15 +7,15 @@ const logger = require("./logger");
 const BASE_PATH = '/familie/alene-med-barn/soknad';
 
 const routes = () => {
-    logger.info(process.env.REACT_APP_BRUK_API_I_DEV)
-    logger.info(process.env.NODE_ENV)
     const expressRouter = express.Router();
 
-    expressRouter.use(BASE_PATH, express.static(buildPath, { index: false }));
-    expressRouter.get(`${BASE_PATH}/internal/isAlive|isReady`, (req, res) =>
+    expressRouter.get(`/internal/isAlive|isReady`, (req, res) =>
         res.sendStatus(200)
     );
-    expressRouter.use(/^(?!.*\/(internal|static)\/).*$/, (req, res) =>
+    expressRouter.use(BASE_PATH, express.static(buildPath, {index: false}));
+
+    expressRouter.use(/^(?!.*\/(internal|static)\/).*$/, (req, res) => {
+        logger.error(`Request path blev ${req.path}`)
         getHtmlWithDecorator((path.join(buildPath, 'index.html')))
             .then((html) => {
                 res.send(html);
@@ -23,7 +23,8 @@ const routes = () => {
             .catch((e) => {
                 logger.error(e)
                 res.status(500).send(e);
-            }))
+            })
+    })
 
     return expressRouter
 }
