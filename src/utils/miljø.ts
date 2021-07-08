a1c2fe98878134e3f34a92104d47d497565474a7
alexandraback <alexandra1.back@gmail.com>
2021-07-08 16:30:21 +0200

    Fjern logging

--- src/utils/miljø.ts
+++ src/utils/miljø.ts
@@ -1,6 +1,4 @@
 export const erLokaltMedMock = () => {
-  console.log(process.env.NODE_ENV)
-  console.log(process.env.REACT_APP_BRUK_API_I_DEV )
   return (
     process.env.NODE_ENV === 'development' &&
     process.env.REACT_APP_BRUK_API_I_DEV === 'false'
