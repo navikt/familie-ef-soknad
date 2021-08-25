53452a95de9077d6befc5336211f9bf41773094b
My Thao Nguyen <my.thao.nguyen@nav.no>
2021-08-25 09:56:09 +0200

    Opprydding og fiks

--- src/overgangsstønad/steg/8-dokumentasjon/SendSøknad.tsx
+++ src/overgangsstønad/steg/8-dokumentasjon/SendSøknad.tsx
@@ -157,7 +157,7 @@ const SendSøknadKnapper: FC = () => {
               }}
             >
               <LocaleTekst tekst="dokumentasjon.alert.link.fylleInn" />
-            </Link>{' '}
+            </Link>
             <LocaleTekst tekst="dokumentasjon.alert.manglende" />
           </AlertStripe>
         </KomponentGruppe>
