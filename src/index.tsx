bd9f8b38cdc9eb777b6f6de2fc6179ac5c6fb0fb
Johan Blomgren <blommish@users.noreply.github.com>
2021-09-01 12:34:34 +0200

    Sbs link til gcp

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,10 @@
 import './utils/polyfills';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.less';
 import * as serviceWorker from './serviceWorker';
-import App from './App';
-import ArbeidssøkerApp from './arbeidssøkerskjema/SkjemaApp';
-import BarnetilsynApp from './barnetilsyn/BarnetilsynApp';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { SpråkProvider } from './context/SpråkContext';
-import ContextProviders from './context/ContextProviders';
-import { ScrollToTop } from './utils/visning';
 import * as Sentry from '@sentry/browser';
 import Environment from './Environment';
-import SkolepengerApp from './skolepenger/SkolepengerApp';
-import DokumentasjonsbehovApp from './dokumentasjonsbehov/DokumentasjonsbehovApp';
 
 if (Environment().sentryUrl) {
   Sentry.init({
@@ -23,20 +14,7 @@ if (Environment().sentryUrl) {
 }
 
 ReactDOM.render(
-  <SpråkProvider>
-    <ContextProviders>
-      <Router basename={process.env.PUBLIC_URL}>
-        <ScrollToTop />
-        <Switch>
-          <Route path={'/arbeidssoker'} component={ArbeidssøkerApp} />
-          <Route path={'/barnetilsyn'} component={BarnetilsynApp} />
-          <Route path={'/skolepenger'} component={SkolepengerApp} />
-          <Route path={'/innsendtsoknad'} component={DokumentasjonsbehovApp} />
-          <Route path={'/'} component={App} />
-        </Switch>
-      </Router>
-    </ContextProviders>
-  </SpråkProvider>,
+    <div style={{position: "fixed", top: "40%", left: "25%"}}>Ny URL er: <a href={'https://familie.dev.nav.no/familie/alene-med-barn/soknad'}>https://familie.dev.nav.no/familie/alene-med-barn/soknad</a></div>,
   document.getElementById('root')
 );
 
