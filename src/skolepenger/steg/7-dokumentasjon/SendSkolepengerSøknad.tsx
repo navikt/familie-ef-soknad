dac3487e9311ab5a355298d6627b0b73b5fdca17
Asbjørn Steinskog <asbjorn@steinskog.me>
2021-10-27 11:11:02 +0200

    Revert "Hindre innsending under oppgradering (#710)"
    This reverts commit 41642aeef8b620cbdc83811cbefbed5b2935cd57.

--- src/skolepenger/steg/7-dokumentasjon/SendSkolepengerSøknad.tsx
+++ src/skolepenger/steg/7-dokumentasjon/SendSkolepengerSøknad.tsx
@@ -5,7 +5,7 @@ import { IStatus } from '../../../arbeidssøkerskjema/innsending/typer';
 import { parseISO } from 'date-fns';
 import { useHistory, useLocation } from 'react-router';
 import KomponentGruppe from '../../../components/gruppe/KomponentGruppe';
-import AlertStripe, { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
+import AlertStripe from 'nav-frontend-alertstriper';
 import { Normaltekst } from 'nav-frontend-typografi';
 import SeksjonGruppe from '../../../components/gruppe/SeksjonGruppe';
 import { StyledKnapper } from '../../../arbeidssøkerskjema/komponenter/StyledKnapper';
@@ -30,7 +30,6 @@ import {
 } from '../../../utils/amplitude';
 import { ESkjemanavn, skjemanavnIdMapping } from '../../../utils/skjemanavn';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 
 interface Innsending {
   status: string;
@@ -41,10 +40,6 @@ interface Innsending {
 const validerSøkerBosattINorgeSisteTreÅr = (søknad: ISøknad) => {
   return søknad.medlemskap.søkerBosattINorgeSisteTreÅr;
 };
-
-const OppgraderingAdvarsel = styled(AlertStripeAdvarsel)`
-  margin-bottom: 1rem;
-`;
 
 const SendSøknadKnapper: FC = () => {
   const { søknad, settSøknad } = useSkolepengerSøknad();
@@ -128,10 +123,6 @@ const SendSøknadKnapper: FC = () => {
           </AlertStripe>
         </KomponentGruppe>
       )}
-      <OppgraderingAdvarsel>
-        Vi oppgraderer for tiden søknadsdialogen. Du kan fylle ut og mellomlagre
-        søknaden din, men du får for øyeblikket ikke sendt inn.
-      </OppgraderingAdvarsel>
       <SeksjonGruppe className={'sentrert'}>
         <StyledKnapper>
           <KnappBase
@@ -144,7 +135,6 @@ const SendSøknadKnapper: FC = () => {
 
           {validerSøkerBosattINorgeSisteTreÅr(søknad) && (
             <KnappBase
-              disabled
               type={'hoved'}
               onClick={() => !innsendingState.venter && sendSøknad(søknad)}
               className={'neste'}
