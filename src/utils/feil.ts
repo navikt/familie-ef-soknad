12fef1f2228a03ffecd055b052fe127f16571695
Johan Blomgren <blommish@users.noreply.github.com>
2021-12-20 11:09:41 +0100

    Kommentar på utilmetode

--- src/utils/feil.ts
+++ src/utils/feil.ts
@@ -4,13 +4,20 @@ const REGEX_FEIL = /^CODE=(.*)$/;
 
 const createId = (prefixId: string, id: string) => `${prefixId}.${id}`;
 
+/**
+ * Genererer feilmelding fra feil fra backend som kan inneholde en kode.
+ * @param intl
+ * @param prefixId eks `filopplaster.feilmelding`
+ * @param defaultSuffixId eks `generisk`, blir satt sammen med prefixId => `filopplaster.feilmelding.generisk`
+ * @param feilmelding eks `CODE=FOO` blir satt sammen med prefixId => `filopplaster.feilmelding.FOO`
+ */
 export const getFeilmelding = (
   intl: IntlShape,
   prefixId: string,
   defaultSuffixId: string,
-  melding?: string
+  feilmelding?: string
 ): string => {
-  const matches = REGEX_FEIL.exec(melding || '');
+  const matches = REGEX_FEIL.exec(feilmelding || '');
   if (matches?.length && matches?.length > 0) {
     const id = createId(prefixId, matches[1]);
     if (intl.messages[id]) {
