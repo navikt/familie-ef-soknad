dde56ab3b096ebde2b189d630e36419860de599a
Johan Blomgren <blommish@users.noreply.github.com>
2022-06-23 12:51:47 +0200

    Rerouter requester for innsendtsoknad til ettersending for å senere kunne slette data koblet til den endepunkten

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,9 @@ const getHtmlWithDecorator = require("./decorator");
 const buildPath = path.resolve(__dirname, "../build");
 const logger = require("./logger");
 
-const BASE_PATH = '/familie/alene-med-barn/soknad';
+const EF_BASE_PATH = '/familie/alene-med-barn';
+const BASE_PATH = `${EF_BASE_PATH}/soknad`;
+const ETTERSENDING_PATH = `${EF_BASE_PATH}/ettersending`;
 
 const routes = () => {
     const expressRouter = express.Router();
@@ -12,6 +14,11 @@ const routes = () => {
     expressRouter.get(`${BASE_PATH}/internal/isAlive|isReady`, (req, res) =>
         res.sendStatus(200)
     );
+
+    expressRouter.get(`${BASE_PATH}/innsendtsoknad/*`, (req, res, next) => {
+        res.redirect(ETTERSENDING_PATH)
+    });
+
     expressRouter.use(BASE_PATH, express.static(buildPath, {index: false}));
 
     expressRouter.use(/^(?!.*\/(internal|static)\/).*$/, (req, res) => {
