f449e2b2af3099c5738f975987d53ee1b27708ae
Johan Blomgren <blommish@users.noreply.github.com>
2022-10-04 16:45:49 +0200

    fikser workflow

--- tests/testContext.ts
+++ tests/testContext.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 import { mockRoutes } from './mockRoutes';
 
 export const testMedApiMocks = test.extend({
-  page: async ({ page }, use) => {
+  page: async ({ page }, use, testInfo) => {
     await mockRoutes(page);
     await use(page);
   },
