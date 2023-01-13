c8ab8eb02a25ea63addf481b887adfa93690b774
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2023-01-13 15:50:20 +0100

    Rettet lokal url for autentisering

--- src/Environment.ts
+++ src/Environment.ts
@@ -47,7 +47,7 @@ const Environment = (): EnvironmentProps => {
     return {
       veiviserUrl: '',
       apiUrl: '',
-      wonderwallUrl: `http://localhost:8091/local/cookie?subject=21057822284&issuerId=idporten&audience=someaudience`, // forventet i api ved innsending (local) - syntetisk fnr
+      wonderwallUrl: `http://localhost:8091/local/cookie?subject=21057822284&issuerId=idporten&audience=familie-app`, // forventet i api ved innsending (local) - syntetisk fnr
       dokumentUrl: `/api/dokument`,
       mellomlagerUrl: `/api/mellomlager/`,
       miljø: 'local',
@@ -57,7 +57,7 @@ const Environment = (): EnvironmentProps => {
     return {
       veiviserUrl: '',
       apiUrl: 'http://localhost:8091',
-      wonderwallUrl: `http://localhost:8091/local/cookie?subject=21057822284&issuerId=idporten&audience=someaudience`, // forventet i api ved innsending (local) - syntetisk fnr
+      wonderwallUrl: `http://localhost:8091/local/cookie?subject=21057822284&issuerId=idporten&audience=familie-app`, // forventet i api ved innsending (local) - syntetisk fnr
       dokumentUrl: `http://localhost:8082/api/mapper/ANYTHING`,
       mellomlagerUrl: `http://localhost:8082/api/soknad/`,
       miljø: 'local',
