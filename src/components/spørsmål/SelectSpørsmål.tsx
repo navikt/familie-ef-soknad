a749d00cd0320ea602fc3c7897d23367d90a0ef1
sarahjelle <sara.hjelle@bekk.no>
2023-08-18 12:56:52 +0200

    Skal fjerne ubrukte vars

--- src/components/spørsmål/SelectSpørsmål.tsx
+++ src/components/spørsmål/SelectSpørsmål.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useEffect } from 'react';
+import { FC } from 'react';
 import { ISpørsmål, ISvar } from '../../models/felles/spørsmålogsvar';
 import LesMerTekst from '../LesMerTekst';
-import { Checkbox, SkjemaGruppe } from 'nav-frontend-skjema';
+import { SkjemaGruppe } from 'nav-frontend-skjema';
 import styled from 'styled-components/macro';
 import Show from '../../utils/showIf';
 import { logSpørsmålBesvart } from '../../utils/amplitude';
