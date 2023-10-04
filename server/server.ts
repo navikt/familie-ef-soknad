5b58f1a0d3f7130c7609d063cddba6c53a88cfbd
ma10s <mattis.janitz@nav.no>
2023-10-04 11:50:46 +0200

    Test csp i preprod

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,19 @@ import express from 'express';
 
 import routes from './routes';
 import cookieParser from 'cookie-parser';
+import {cspString} from "../src/csp";
 const app = express();
+
+app.use((_req, res, next) => {
+
+  res.header(
+      'Content-Security-Policy',
+      cspString(process.env.DEKORATOREN_URL ?? 'https://www.nav.no/dekoratoren')
+  );
+  res.header('X-Content-Type-Options', 'nosniff');
+  res.header('X-Frame-Options', 'DENY');
+  next();
+});
 
 if (process.env.ENV === 'localhost') {
   app.use(cookieParser());
