2ecf69f126ad1b7f58e578be7fcf158715fe4786
Charlie Midtlyng <charlie.midtlyng@bekk.no>
2024-01-29 15:29:46 +0100

    Revert "Revert "Skal kunne kjøre opp mot preprod fra lokal kjøring. Kjører også via s…""

--- server/server.ts
+++ server/server.ts
@@ -1,24 +1,15 @@
 import express from 'express';
 
 import routes from './routes';
-import cookieParser from 'cookie-parser';
-import {cspString} from './csp';
+import { cspString } from './csp';
 const app = express();
 
 app.use((_req, res, next) => {
-
-  res.header(
-      'Content-Security-Policy',
-      cspString()
-  );
+  res.header('Content-Security-Policy', cspString());
   res.header('X-Content-Type-Options', 'nosniff');
   res.header('X-Frame-Options', 'DENY');
   next();
 });
-
-if (process.env.ENV === 'localhost') {
-  app.use(cookieParser());
-}
 app.use(routes());
 
 app.listen(8080);
--- server/tokenProxy.ts
+++ server/tokenProxy.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import TokenXClient from './tokenx';
 import { logWarn, logInfo } from './logger';
+import { miljø } from './miljø';
 
 const { exchangeToken } = new TokenXClient();
 
@@ -16,7 +17,9 @@ const attachToken = (applicationName: ApplicationName): RequestHandler => {
         req,
         applicationName
       );
-      req.headers[AUTHORIZATION_HEADER] = authenticationHeader.authorization;
+      req.headers[AUTHORIZATION_HEADER] = brukCookie()
+        ? ''
+        : authenticationHeader.authorization;
       req.headers[WONDERWALL_ID_TOKEN_HEADER] = '';
       next();
     } catch (error) {
@@ -32,18 +35,14 @@ const attachToken = (applicationName: ApplicationName): RequestHandler => {
   };
 };
 
-const erLokalt = () => {
-  return process.env.ENV === 'localhost';
-};
+const brukCookie = () => miljø.erLokalt && !miljø.brukDevApi;
 
 const harBearerToken = (authorization: string) => {
   return authorization.includes('Bearer ');
 };
 
 const utledToken = (req: Request, authorization: string | undefined) => {
-  if (erLokalt()) {
-    return req.cookies['localhost-idtoken'];
-  } else if (authorization && harBearerToken(authorization)) {
+  if (authorization && harBearerToken(authorization)) {
     return authorization.split(' ')[1];
   } else {
     throw Error('Mangler authorization i header');
@@ -55,6 +54,15 @@ const prepareSecuredRequest = async (
   applicationName: ApplicationName
 ) => {
   logInfo('PrepareSecuredRequest', req);
+  if (miljø.erLokalt) {
+    const lokalToken =
+      applicationName === 'familie-ef-soknad-api'
+        ? miljø.lokaltTokenxApi
+        : miljø.lokaltTokenxDokument;
+    return {
+      authorization: `Bearer ${lokalToken}`,
+    };
+  }
   const { authorization } = req.headers;
   const token = utledToken(req, authorization);
   logInfo('IdPorten-token found: ' + (token.length > 1), req);
