29ff29a39668b3d6e55007b71dc1f420f189ba6f
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2024-03-19 08:56:56 +0100

    Token support 4 støtter ikke token i cookie (#1635)
    * Hent token fra fakedings ved lokal kjøring
    * Hent token med fetch for å unngå avhengighet til axios i server

--- server/tokenProxy.ts
+++ server/tokenProxy.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, RequestHandler, Response } from 'express';
 import TokenXClient from './tokenx';
 import { logWarn, logInfo } from './logger';
 import { miljø } from './miljø';
+import axios from 'axios';
 
 const { exchangeToken } = new TokenXClient();
 
@@ -13,13 +14,10 @@ const WONDERWALL_ID_TOKEN_HEADER = 'x-wonderwall-id-token';
 const attachToken = (applicationName: ApplicationName): RequestHandler => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const authenticationHeader = await prepareSecuredRequest(
-        req,
-        applicationName
-      );
-      req.headers[AUTHORIZATION_HEADER] = brukCookie()
-        ? ''
-        : authenticationHeader.authorization;
+      req.headers[AUTHORIZATION_HEADER] =
+        miljø.erLokalt && !miljø.brukDevApi
+          ? await getFakedingsToken(applicationName)
+          : await getAccessToken(req, applicationName);
       req.headers[WONDERWALL_ID_TOKEN_HEADER] = '';
       next();
     } catch (error) {
@@ -35,8 +33,6 @@ const attachToken = (applicationName: ApplicationName): RequestHandler => {
   };
 };
 
-const brukCookie = () => miljø.erLokalt && !miljø.brukDevApi;
-
 const harBearerToken = (authorization: string) => {
   return authorization.includes('Bearer ');
 };
@@ -49,19 +45,17 @@ const utledToken = (req: Request, authorization: string | undefined) => {
   }
 };
 
-const prepareSecuredRequest = async (
+const getAccessToken = async (
   req: Request,
   applicationName: ApplicationName
-) => {
+): Promise<string> => {
   logInfo('PrepareSecuredRequest', req);
   if (miljø.erLokalt) {
     const lokalToken =
       applicationName === 'familie-ef-soknad-api'
         ? miljø.lokaltTokenxApi
         : miljø.lokaltTokenxDokument;
-    return {
-      authorization: `Bearer ${lokalToken}`,
-    };
+    return `Bearer ${lokalToken}`;
   }
   const { authorization } = req.headers;
   const token = utledToken(req, authorization);
@@ -69,9 +63,17 @@ const prepareSecuredRequest = async (
   const accessToken = await exchangeToken(token, applicationName).then(
     (accessToken) => accessToken
   );
-  return {
-    authorization: `Bearer ${accessToken}`,
-  };
+  return `Bearer ${accessToken}`;
+};
+
+const getFakedingsToken = async (applicationName: string): Promise<string> => {
+  const clientId = 'dev-gcp:teamfamilie:familie-ef-soknad';
+  const audience = `dev-gcp:teamfamilie:${applicationName}`;
+  const url = `https://fakedings.intern.dev.nav.no/fake/tokenx?client_id=${clientId}&aud=${audience}&acr=Level4&pid=31458931375`;
+  const token = await fetch(url).then(function (body) {
+    return body.text();
+  });
+  return `Bearer ${token}`;
 };
 
 export default attachToken;
