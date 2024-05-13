453674831dbf2dc14b448aaf9ee087d5bc597f10
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2024-05-13 14:38:56 +0200

    Kommentert hvorfor z-index blir brukt

--- src/assets/VeilederSnakkeboble.tsx
+++ src/assets/VeilederSnakkeboble.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { GuidePanel } from '@navikt/ds-react';
 import styled from 'styled-components';
 
+/*
+ * z-index er lagt på for at språkvelgeren skal legge seg over GuidePanel
+ * slik at det er mulig å velge språk på mindre skjermer.
+ */
 const StyledGuidePanel = styled(GuidePanel)`
   z-index: -1;
 `;
