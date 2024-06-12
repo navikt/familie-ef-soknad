5c8c2175e73d47ae652970dee36214abc5d070a5
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2024-06-12 12:53:28 +0200

    Lagt validering tilbake på BT søknad. Refaktorert validering + lagt til datofelter som manglet. Laget komponenten ManglendeFelterAlert da det er kode som brukes flere steder.

--- src/barnetilsyn/steg/7-oppsummering/Oppsummering.tsx
+++ src/barnetilsyn/steg/7-oppsummering/Oppsummering.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import KomponentGruppe from '../../../components/gruppe/KomponentGruppe';
 import OppsummeringAktiviteter from '../../../søknad/steg/7-oppsummering/OppsummeringAktiviteter';
 import OppsummeringBarnaDine from '../../../søknad/steg/7-oppsummering/OppsummeringBarnaDine';
@@ -17,9 +17,21 @@ import { hentPath } from '../../../utils/routing';
 import Side, { ESide } from '../../../components/side/Side';
 import { hentTekst } from '../../../utils/søknad';
 import { Stønadstype } from '../../../models/søknad/stønadstyper';
-import { logSidevisningBarnetilsyn } from '../../../utils/amplitude';
+import {
+  logManglendeFelter,
+  logSidevisningBarnetilsyn,
+} from '../../../utils/amplitude';
 import { useMount } from '../../../utils/hooks';
 import { Accordion, BodyShort } from '@navikt/ds-react';
+import {
+  ManglendeFelter,
+  aktivitetSchemaBT,
+  manglendeFelterTilTekst,
+  medlemskapSchema,
+  sivilstatusSchema,
+} from '../../../utils/validering/validering';
+import { ESkjemanavn, skjemanavnIdMapping } from '../../../utils/skjemanavn';
+import ManglendeFelterAlert from '../../../components/feil/ManglendeFelterAlert';
 
 const Oppsummering: React.FC = () => {
   const intl = useLokalIntlContext();
@@ -27,8 +39,63 @@ const Oppsummering: React.FC = () => {
   const barnSomSkalHaBarnepass: IBarn[] = søknad.person.barn.filter(
     (barn: IBarn) => barn.skalHaBarnepass?.verdi
   );
+  const skjemaId = skjemanavnIdMapping[ESkjemanavn.Barnetilsyn];
+  const [manglendeFelter, settManglendeFelter] = useState<string[]>([]);
 
   useMount(() => logSidevisningBarnetilsyn('Oppsummering'));
+
+  const oppdaterManglendeFelter = (manglendeFelt: ManglendeFelter) => {
+    settManglendeFelter((prev: string[]): string[] => [
+      ...prev,
+      manglendeFelterTilTekst[manglendeFelt],
+    ]);
+  };
+
+  const feilIkkeRegistrertFor = (felt: ManglendeFelter) => {
+    return !manglendeFelter.includes(manglendeFelterTilTekst[felt]);
+  };
+
+  useEffect(() => {
+    aktivitetSchemaBT
+      .validate(søknad.aktivitet)
+      .then()
+      .catch((e) => {
+        if (
+          !manglendeFelter.includes(
+            manglendeFelterTilTekst[ManglendeFelter.AKTIVITET]
+          )
+        ) {
+          settManglendeFelter((prev: string[]): string[] => [
+            ...prev,
+            manglendeFelterTilTekst[ManglendeFelter.AKTIVITET],
+          ]);
+        }
+
+        logManglendeFelter(ESkjemanavn.Barnetilsyn, skjemaId, e);
+      });
+
+    sivilstatusSchema
+      .validate(søknad.sivilstatus)
+      .then()
+      .catch((e) => {
+        if (feilIkkeRegistrertFor(ManglendeFelter.OM_DEG)) {
+          oppdaterManglendeFelter(ManglendeFelter.OM_DEG);
+        }
+        logManglendeFelter(ESkjemanavn.Overgangsstønad, skjemaId, e);
+      });
+
+    medlemskapSchema
+      .validate(søknad.medlemskap)
+      .then()
+      .catch((e) => {
+        if (feilIkkeRegistrertFor(ManglendeFelter.OM_DEG)) {
+          oppdaterManglendeFelter(ManglendeFelter.OM_DEG);
+        }
+        logManglendeFelter(ESkjemanavn.Overgangsstønad, skjemaId, e);
+      });
+  }, [søknad, manglendeFelter, skjemaId]);
+
+  const harManglendeFelter = manglendeFelter.length > 0;
 
   return (
     <>
@@ -39,6 +106,7 @@ const Oppsummering: React.FC = () => {
         mellomlagreStønad={mellomlagreBarnetilsyn}
         routesStønad={RoutesBarnetilsyn}
         skalViseKnapper={ESide.visTilbakeNesteAvbrytKnapp}
+        disableNesteKnapp={harManglendeFelter}
       >
         <div className="oppsummering">
           <BodyShort className="disclaimer">
@@ -145,6 +213,10 @@ const Oppsummering: React.FC = () => {
               </Accordion.Item>
             </Accordion>
           </KomponentGruppe>
+
+          {harManglendeFelter && (
+            <ManglendeFelterAlert manglendeFelter={manglendeFelter} />
+          )}
         </div>
       </Side>
     </>
